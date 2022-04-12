57610f90a6c5b745cb594450d601d0b3442ed134
Arturo Vazquez <avazqu48@uic.edu>
2022-04-12 18:50:02 -0500

    Favicon changed

--- components/Result.js
+++ components/Result.js
@@ -8,14 +8,18 @@ export default function Result(props) {
   return (
 
     <>
-        <div  className={styles.modal} style={{display: props.modalState}}>
+        <div className={styles.modal} style={{display: props.modalState}}>
 
             {/* <!-- Modal content --> */}
             <div className={styles.modalContent}>
                 <span className={styles.close} onClick={() => {props.setModalState('none'); router.reload('/');}}>&times;</span>
                 <h1>Typing Test Complete!</h1>
-                {/* <h2>You typed the 1 Minute Typing Test.</h2> */}
-                <h3>Your speed was {props.wpm} WPM ({props.cpm} CPM) with accuracy {props.accuracy}%</h3>
+                
+                <h2>{props.wpm} WPM</h2>
+                <h2>{props.cpm} CPM</h2>
+                <h2>{props.accuracy}% ACCURACY</h2>
+
+                <button onClick={() => router.reload('/')}>TRY AGAIN</button>
             </div>
         </div>
     </>
